58f5d3cdee67f451f3f1a8fa896b742b3986f35b
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-17 14:41:26 +0300

    created PlaceFinder class in SharePlace.js and got access to the form and button in index.html from the root in dist folder

---
+++ src/SharePlace.js
@@ -0,0 +1,13 @@
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+
+    locateUserBtn.addEventListener('click', this.locateUserHandler);
+    addressForm.addEventListener('submit', this.findAddressHandler);
+  }
+
+  locateUserHandler() {}
+
+  findAddressHandler() {}
+}
