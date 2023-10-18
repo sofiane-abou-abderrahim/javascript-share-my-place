4dac982c18525798aef7452c9d7a77933615fd2d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-18 12:17:50 +0300

    commented about how we built the hide method

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -10,23 +10,37 @@ export class Modal {
         this.modalTemplateEl.content,
         true
       );
-      this.modalElement = modalElements.querySelector('.modal');
-      this.backdropElement = modalElements.querySelector('.backdrop');
+      this.modalElement = modalElements.querySelector('.modal'); // replaced "const" by "this"
+      this.backdropElement = modalElements.querySelector('.backdrop'); // replaced "const" by "this"
       const contentElement = document.importNode(
         this.contentTemplateEl.content,
         true
       );
 
       this.modalElement.appendChild(contentElement);
+      // therefore, we need to add "this" here, because these are no longer variables or constants of this method,
+      // these are now properties of the class instance.
 
       document.body.insertAdjacentElement('afterbegin', this.modalElement);
+      // therefore, we need to add "this" here, because these are no longer variables or constants of this method,
+      // these are now properties of the class instance.
       document.body.insertAdjacentElement('afterbegin', this.backdropElement);
+      // therefore, we need to add "this" here, because these are no longer variables or constants of this method,
+      // these are now properties of the class instance.
     } else {
       // fallback code
       alert(this.fallbackText);
     }
   }
 
+  /*
+
+  To hide the modal, we basically need to remove the backdropElement and the modalElement from the DOM.
+  To remove these elements here, I will store modalElement in a property not in a constant and the same here for backdropElement,
+  so that they're are not just constants that are available in the show() method, 
+  but properties of the entire Modal class or of the instance we create based on the class to be precise
+
+  */
   hide() {
     if (this.modalElement) {
       document.body.removeChild(this.modalElement); // this.modalElement.remove()
